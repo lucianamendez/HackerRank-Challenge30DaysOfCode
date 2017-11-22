13b9662b0a3d9b3306bce4bae2a28ba6ac78f906
Luciana Mendez <dg.lucianamendez@gmail.com>
2017-11-21 23:46:22 -0300

    add rs

---
+++ Day10_BinaryNumbers.java
@@ -0,0 +1,34 @@
+/*Given a base-10 integer,n, convert it to binary (base-2). Then find and print the base-10 integer 
+denoting the maximum number of consecutive 1's in n's binary representation.*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        int extra;
+        int contador=0;
+        int t=0;
+        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+    
+        while(n>0){
+            extra=n%2;
+            n=n/2;
+                if (extra == 1){
+                    contador++;
+                    if (contador>=t){
+                        t=contador;
+                    }
+                }
+                else{
+                    contador=0;
+                }
+            }        
+                
+        System.out.println(t);
+    }
+}
